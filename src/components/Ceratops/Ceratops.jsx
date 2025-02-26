80e1dfbcde1e728e783a11a75855c9cb7cb148c1
oleksiiivanov <oleksii@bitmotive.com>
2025-02-26 21:31:42 +0100

    Added link

---
+++ src/components/Ceratops/Ceratops.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Ceratops = () => (
+  <div className="ceratops">
+    <p>123</p>
+  </div>
+);
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from '@/assets/header-logo.svg';
 import './Header.scss';
+import { Link } from 'react-router-dom';
 
 export const Header = () => (
   <header className="header">
     <div className="header__background" />
     <img className="header__logo" src={logo} alt="logo" />
-    <div className="header__container">
+    <Link to="/" className="header__container">
       <p className="header__title">Lunétique</p>
       <p className="header__author">by Vlad Mozharovsky</p>
-    </div>
+    </Link>
   </header>
 );
