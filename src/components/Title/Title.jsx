c0c63d6b6886bb7aff7b2a3d186726fff4164ffc
Rybytska <miss.anastasi98@gmail.com>
2025-02-27 22:24:39 +0100

    whole mainpage

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,9 +1,35 @@
+import './Title.scss';
+import star from '@/assets/star-white.svg';
+
 export const Title = () => {
   console.log(123);
 
   return (
-    <div>
-      <p>title</p>
+    <div className="title">
+      <div className="title__up">
+        <img src={star} alt="" />
+        <p>Functional brilliance</p>
+      </div>
+      <div className="title__title">
+        <p className="title__title1">Design Boutique</p>
+        <p className="title__title2">of Digital Excellence</p>
+      </div>
+      <div className="title__text">
+        <p>
+          From creative web aesthetics to shaping high-converting digital
+          products. Focused on SaaS, AI & Web3 Products.
+        </p>
+      </div>
+      <button className="transform__button">
+        <div className="transform__circle">
+          <p>Contact me</p>
+          {/* <img src={star} alt="" /> */}
+        </div>
+      </button>
+      <div>
+        <p>Experience working with the best minds on the planet</p>
+        {/* <div></div> */}
+      </div>
     </div>
   );
 };
--- src/components/Transform/Transform.jsx
+++ src/components/Transform/Transform.jsx
@@ -1,9 +1,36 @@
+import './Transform.scss';
+import arrow from '@/assets/arrow right.svg';
+
 export const Transform = () => {
   console.log(123);
 
   return (
-    <div>
-      <p>Transform</p>
+    <div className="transform">
+      <div className="transform__shadows">
+        <div className="transform__shadow" />
+        <div className="transform__shadow" />
+        <div className="transform__shadow" />
+        <div className="transform__shadow" />
+        <div className="transform__shadow" />
+        <div className="transform__shadow" />
+      </div>
+      <div className="transform__container">
+        <div className="transform__text">
+          <p className="transform__up">Transform Your Vision into Reality</p>
+          <p className="transform__bottom">
+            Let’s talk and translate your ideas into impactful designs today.
+          </p>
+          <p className="transform__bottom">
+            The future doesn't wait, why should you?
+          </p>
+        </div>
+        <button className="transform__button">
+          <div className="transform__circle">
+            <p>Contact me</p>
+            <img src={arrow} alt="" />
+          </div>
+        </button>
+      </div>
     </div>
   );
 };
