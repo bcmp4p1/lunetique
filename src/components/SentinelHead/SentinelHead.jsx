167ea1d35124fcadbfb781ecadc6dd780d4227df
Rybytska <miss.anastasi98@gmail.com>
2025-03-02 18:04:48 +0100

    3rd page

---
+++ src/components/SentinelHead/SentinelHead.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// import logo from '@/assets/ceratops.png';
+// import designOne from '@/assets/ceratops1.png';
+
+export const SentinelHead = () => (
+  <div className="sentinel__head">
+   <p>ffje</p>
+  </div>
+);
